f66583433de9fc2d3509610e42e060783c39a58b
Denis Lom <lom.denis@seznam.cz>
2024-11-21 02:00:58 +0100

    2/5

--- Simple Tic-Tac-Toe (Kotlin)/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe (Kotlin)/task/src/tictactoe/Main.kt
@@ -2,9 +2,19 @@ package tictactoe
 
 fun main() {
     // write your code here
-    print("""
-X O X
-O X O
-X X O
-    """)
+    val input: String = readln()
+    var i = 0
+    println("---------")
+//    print("|")
+    while (i < input.length) {
+//        print(i)
+        if (i == 0)
+            print("| ${input[i++]}")
+        else if (i % 3 == 0)
+            print(" |\n| ${input[i++]}")
+        else (i % 3 != 0)
+            print(" ${input[i++]}")
+    }
+    println(" |")
+    println("---------")
 }
